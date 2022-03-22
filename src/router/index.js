1582252274417068659f051c9c4e9c349e9ffb54
Henrique Noronha Favorette <henrique.nofv@gmail.com>
2022-03-22 08:48:47 -0300

    revert routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,19 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>import( '@/components/HomeIndex.vue')
+    },
+    {
+      path: '/app',
     name: 'RouterView',
     component: () =>import( '@/views/RouterView.vue'),
     children: [
       {
         path: '/',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>import( '@/components/HomeIndex.vue')
-      },
-      {
-        path: '/app',
         name: 'Campo',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
